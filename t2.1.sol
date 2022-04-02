2c2da3cd5226f58bbd41a18f49b9163553db435a
xP1ore <90790203+xP1ore@users.noreply.github.com>
2022-04-02 13:32:30 +0300

    Add files via upload

---
+++ t2.1.sol
@@ -0,0 +1,61 @@
+//"SPDX-License-Identifier: UNLICENSED"
+pragma solidity 0.8.6;
+
+// массив - упорядоченный набор данных одного типа
+// виды массивов: статический/динамический (фиксированной/нефиксированной длины); массив чисел; массив строк; соответсвие (mapping); структура (struct); однемерный(вектор)/двумерный(матрица)
+contract task2 {
+    uint32[] a; //динамический однемерный массив
+    uint32[2][4] b = [[22,3], [1,2], [12,12], [3,4]]; //статический двумерный массив
+    string[] lines; // массив строк
+    
+
+    function getLinesCount() public view returns (uint) {
+        return lines.length;
+    }
+
+    function addLine(string memory _s) public {
+        lines.push(_s);
+    }
+
+    function getLines() public view returns (string[] memory) {
+        return lines;
+    }
+
+    function removeLastLine() public {
+        lines.pop();
+    }
+    
+    function getArrayLen() public view returns (uint) {
+        return a.length;
+    }
+    
+    function addNum(uint32 _n) public {
+        a.push(_n);
+    }
+    
+    function removeLastElem() public {
+        a.pop();    
+    }
+    
+    function getArray() public view returns (uint32[] memory) {
+        return a;
+    }
+    
+    function removeSpecElem(uint16 _index) public returns (string memory) {
+        string memory warn = "element was removed successfully";
+        if (_index >= a.length) {
+            warn = "index is out of length";
+            return warn;
+        }
+        else {
+            delete a[_index];
+            
+            for (uint i = _index; i < a.length-1; i++) {
+                a[i] = a[i+1];
+            }
+            
+            a.pop();
+            return warn;
+        }
+    }
+}
